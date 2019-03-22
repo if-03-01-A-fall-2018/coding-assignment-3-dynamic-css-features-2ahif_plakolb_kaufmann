de5fdd0de6ba57f5fc18003e7460602069efa58b
Kaufmann Jan <if170063@WS8A16.htl-leonding.ac.at>
2019-03-22 08:22:30 +0100

    added test code to try marker

--- Augmented_Reality/JavaScript/marker_switching.js
+++ Augmented_Reality/JavaScript/marker_switching.js
@@ -1,18 +1,17 @@
 var current_index = 0;
 function marker_switch(direction)
 { //direction == 0 => right; direction == 1 => left;
-  //<script src="../JavaScript/marker_switching.js"></script>
   var marker_paths= ["markers\\english_class_goes_ar-marker.png", "markers\\ar.js-marker.png", "markers\\stonehenge_marker.png", "markers\\hiro_marker_image.png"]
   if(direction == 0)
   {
     if(current_index + 1 > marker_paths.length - 1)
     {
-      console.log("current_index > marker_paths.length")
+      //console.log("current_index > marker_paths.length")
       current_index = 0;
     }
     else
     {
-      console.log("current_index++;")
+      //console.log("current_index++;")
       current_index++;
     }
   }
@@ -20,15 +19,18 @@ function marker_switch(direction)
   {
     if(current_index - 1 < 0)
     {
-      console.log("current_index < 0")
+      //console.log("current_index < 0")
       current_index = marker_paths.length - 1;
     }
     else
     {
-      console.log("current_index--;")
+      //console.log("current_index--;")
       current_index--;
     }
   }
-  console.log("changes path");
+  //console.log("changes path");
   document.getElementById('marker').src = marker_paths[current_index];
 }
+
+anchorRef.addEventListener("markerFound", (e)=>{ console.log("Found marker!")}
+anchorRef.addEventListener("markerLost", (e)=>{ console.log("Lost marker!")}
