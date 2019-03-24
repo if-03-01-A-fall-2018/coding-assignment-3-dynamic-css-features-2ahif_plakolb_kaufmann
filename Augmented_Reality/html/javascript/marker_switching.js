1910c3eb911a39a965ed42e7adb803eb8adc1013
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-03-24 11:21:03 +0100

    added marker + model

--- Augmented_Reality/html/javascript/marker_switching.js
+++ Augmented_Reality/html/javascript/marker_switching.js
@@ -1,7 +1,7 @@
 var current_index = 0;
 function marker_switch(direction)
 { //direction == 0 => right; direction == 1 => left;
-  var marker_paths= ["markers\\english_class_goes_ar-marker.png", "markers\\ar.js-marker.png", "markers\\stonehenge_marker.png", "markers\\hiro_marker_image.png"]
+  var marker_paths= ["markers\\english_class_goes_ar-marker.png", "markers\\ar.js-marker.png", "markers\\stonehenge_marker.png", "markers\\hiro_marker_image.png", "markers\\sphinx_marker.png"]
   if(direction == 0)
   {
     if(current_index + 1 > marker_paths.length - 1)
