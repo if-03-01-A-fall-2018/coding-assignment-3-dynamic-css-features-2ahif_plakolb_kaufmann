17173a8c084d753dae08f14ab76b4274189e01e6
Kaufmann Jan <if170063@WS8A16.local>
2019-05-06 08:01:02 +0200

    .

---
+++ Augmented_Reality/html/javascript/change_rotation.js
@@ -0,0 +1,11 @@
+// Changes from printed marker based to host marker based
+function changeRotation() {
+    document.getElementById("myCheck").checked = false;
+      if(document.getElementById("myCheck").checked)
+      {
+        console.log("Works");
+      }
+      else {
+        console.log("dasd");
+      }
+}
