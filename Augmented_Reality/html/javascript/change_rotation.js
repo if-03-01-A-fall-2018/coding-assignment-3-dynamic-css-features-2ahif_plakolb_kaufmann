8a1886802e0e5d0fb5675ead282d6f428280383c
Kaufmann Jan <if170063@WS8A16.local>
2019-05-20 08:33:03 +0200

    checkbox working

--- Augmented_Reality/html/javascript/change_rotation.js
+++ Augmented_Reality/html/javascript/change_rotation.js
@@ -1,13 +1,20 @@
 // Changes from printed marker based to host marker based
 function changeRotation() {
   // Get the checkbox
-  var checkBox = document.getElementById("button1");
-
+  var check_box = document.getElementById("checkbox");
+  var host_button = document.getElementById("hostbutton");
   // If the checkbox is checked, display the output text
-  if (checkBox.checked == true){
-    text.style.display = "block";
-    console.log("Checkbox is Checked");
-  } else {
-    text.style.display = "none";
+  if (check_box.checked == true)
+  {
+    text1.style.display = "block";
+    text2.style.display = "block";
+    host_button.href = "#";
+    console.log("Checkbox  Checked");
+  }
+  else
+  {
+    text1.style.display = "none";
+    text2.style.display = "none";
+    host_button.href = "web_app_host.html";
   }
 }
