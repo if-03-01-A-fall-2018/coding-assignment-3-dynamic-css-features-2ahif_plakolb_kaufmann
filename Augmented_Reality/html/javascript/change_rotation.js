f6f776bf6631619e522da619e2b304689369e877
Kaufmann Jan <if170063@WS8A16.local>
2019-05-20 08:46:30 +0200

    Improved

--- Augmented_Reality/html/javascript/change_rotation.js
+++ Augmented_Reality/html/javascript/change_rotation.js
@@ -3,18 +3,19 @@ function changeRotation() {
   // Get the checkbox
   var check_box = document.getElementById("checkbox");
   var host_button = document.getElementById("hostbutton");
+  var host_href = "web_app_host.html";
   // If the checkbox is checked, display the output text
   if (check_box.checked == true)
   {
     text1.style.display = "block";
     text2.style.display = "block";
-    host_button.href = "#";
-    console.log("Checkbox  Checked");
+    host_button.href = "#";           //Disable host button
+    console.log("Checkbox checked");
   }
   else
   {
     text1.style.display = "none";
     text2.style.display = "none";
-    host_button.href = "web_app_host.html";
+    host_button.href = host_href;   //Enable Disable host button
   }
 }
