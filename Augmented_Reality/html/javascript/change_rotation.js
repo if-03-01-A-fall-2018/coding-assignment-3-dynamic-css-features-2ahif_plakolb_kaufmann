03d8fb001880cb20a406208fe0649834ec7643cf
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-06-03 08:43:45 +0200

    marker switching 1

--- Augmented_Reality/html/javascript/change_rotation.js
+++ Augmented_Reality/html/javascript/change_rotation.js
@@ -1,16 +1,22 @@
 // Changes from printed marker based to host marker based
 function change_rotation() {
   console.log("test");
-  if (confirm('Press "OK" if you want to use Host markers, press "Abort" if you want to use printed markers!')) {
+  if (confirm('Press "OK" if you want to use Printed markers, press "Abort" if you want to use host markers!')) {
     console.log("Changing...");
     var entityEl = document.querySelector('#trex');
     console.log(entityEl.getAttribute('rotation'));
-    entityEl.object3D.rotation.y = THREE.Math.degToRad(45);
-    entityEl.object3D.rotation.x = THREE.Math.degToRad(45);
-    entityEl.object3D.rotation.z = THREE.Math.degToRad(45);
+    entityEl.object3D.rotation.y = THREE.Math.degToRad(0);
+    entityEl.object3D.rotation.x = THREE.Math.degToRad(0);
+    entityEl.object3D.rotation.z = THREE.Math.degToRad(0);
     console.log(entityEl.getAttribute('rotation'));
+    var entityEl = document.querySelector('#sphinx');
+    entityEl.object3D.rotation.y = THREE.Math.degToRad(270);
+    entityEl.object3D.rotation.x = THREE.Math.degToRad(270);
+    entityEl.object3D.rotation.z = THREE.Math.degToRad(90);
     var entityEl = document.querySelector('#stonehenge');
-    console.log(entityEl);
+    entityEl.object3D.rotation.y = THREE.Math.degToRad(270);
+    entityEl.object3D.rotation.x = THREE.Math.degToRad(0);
+    entityEl.object3D.rotation.z = THREE.Math.degToRad(0);
   } else {
     // Do nothing!
   }
