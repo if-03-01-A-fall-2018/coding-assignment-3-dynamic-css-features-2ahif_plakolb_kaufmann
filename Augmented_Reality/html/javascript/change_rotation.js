937476505ea422190633c75c992a9f0db35622f9
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-06-05 16:21:14 +0200

    i bims fertig mid de nerven

--- Augmented_Reality/html/javascript/change_rotation.js
+++ Augmented_Reality/html/javascript/change_rotation.js
@@ -8,13 +8,16 @@ function change_rotation() {
     entityEl.object3D.rotation.y = THREE.Math.degToRad(0);
     entityEl.object3D.rotation.x = THREE.Math.degToRad(0);
     entityEl.object3D.rotation.z = THREE.Math.degToRad(0);
-    console.log(entityEl.getAttribute('rotation'));
     var entityEl = document.querySelector('#sphinx');
     entityEl.object3D.rotation.y = THREE.Math.degToRad(270);
     entityEl.object3D.rotation.x = THREE.Math.degToRad(270);
     entityEl.object3D.rotation.z = THREE.Math.degToRad(90);
     var entityEl = document.querySelector('#stonehenge');
     entityEl.object3D.rotation.y = THREE.Math.degToRad(270);
+    entityEl.object3D.rotation.x = THREE.Math.degToRad(0);
+    entityEl.object3D.rotation.z = THREE.Math.degToRad(0);
+    var entityEl = document.querySelector('#jupiter');
+    entityEl.object3D.rotation.y = THREE.Math.degToRad(0);
     entityEl.object3D.rotation.x = THREE.Math.degToRad(0);
     entityEl.object3D.rotation.z = THREE.Math.degToRad(0);
   } else {
